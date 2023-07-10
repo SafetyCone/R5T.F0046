3c12434e5359f8e533e3866e78eca11b57fc9f43
davidcoats <david@getsafetycone.com>
2023-07-10 13:26:11 -0700

    Interim changes.

--- source/R5T.F0046/Code/Functionality/IRepositoryNameOperator.cs
+++ source/R5T.F0046/Code/Functionality/IRepositoryNameOperator.cs
@@ -36,7 +36,7 @@ namespace R5T.F0046
 		/// Get the repository name using the owner name and Natter repository 4-digit code.
 		/// Example: "R5T.F0033" is the result of the "SafetyCone" owner (which is turned into the organization namespace token "R5T") and "F0033".
 		/// </summary>
-		/// <param name="owner">The owner name from <see cref="IGitHubOwners"/> recognized by <see cref="IGitHubOwnerOperations.GetOrganizationNamespaceToken(string)"/>. Example: "SafetyCone".</param>
+		/// <param name="owner">The owner name from <see cref="F0047.IGitHubOwners"/> recognized by <see cref="F0047.IGitHubOwnerOperator.GetOrganizationNamespaceToken(string)"/>. Example: "SafetyCone".</param>
 		/// <param name="natterRepositoryCode">The repository code. Example: "F0033"></param>
 		/// <returns>The repository name. Example: "R5T.F0033".</returns>
 		public string GetRepositoryName(
